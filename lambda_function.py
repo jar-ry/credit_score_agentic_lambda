826e634229187b2ae1e5a7b42ff3a2881c818e96
unknown <jarrry.c@gmail.com>
2025-02-26 22:58:36 +1100

    Test lambda

--- lambda_function.py
+++ lambda_function.py
@@ -2,10 +2,5 @@ import json
 
 def lambda_handler(event, context):
     return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "POST"
-        },
         "body": json.dumps({"message": "hi"})
     }

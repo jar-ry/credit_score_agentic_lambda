57e02be06f9b3730b4f30cbbda9e78d526395127
unknown <jarrry.c@gmail.com>
2025-03-05 23:00:06 +1100

    Remove cor header

--- lambda_function.py
+++ lambda_function.py
@@ -249,7 +249,6 @@ def lambda_handler(event, context):
             "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
             },
             "body": json.dumps(response)
         }
